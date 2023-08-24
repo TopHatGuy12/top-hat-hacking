1eb23c0e5e06290beea22a3e35fa52a210240843
Riley Campbell <116246538+RileyCampbell2007@users.noreply.github.com>
2023-08-24 10:10:40 -0600

    Create bookmarklet.js

---
+++ Javascript/bookmarklet.js
@@ -0,0 +1,18 @@
+setInterval(()=> {
+  var bookmarkletElements = Array.from(document.querySelectorAll('bookmarklet'))
+  
+  bookmarkletElements.forEach(async (element) => {
+      if(element.getAttribute('handled') != true) {
+        file = await (await fetch(element.getAttribute('file'))).text()
+        var bookmarkletURI = `javascript:${urlEncodeString(`(function(){${file}})()`)}`
+        var oldHTML = element.innerHTML
+        var newLink = document.createElement('a')
+        newLink.href = bookmarkletURI
+        newLink.setAttribute('onclick','return false;')
+        newLink.innerHTML = oldHTML
+        element.innerHTML = ''
+        element.appendChild(newLink)
+        element.setAttribute('handled', true)
+      }
+  });
+}, 1000)
