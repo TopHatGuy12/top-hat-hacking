58260f5993db8fb10e7fd85e0fb6b24a7a7d57b5
Riley Campbell <116246538+RileyCampbell2007@users.noreply.github.com>
2023-08-24 10:47:27 -0600

    Create infinite-rick-roll.js

---
+++ projects/bookmarklets/bookmarklet-files/infinite-rick-roll.js
@@ -0,0 +1,9 @@
+constlinks = document.getElementsByTagName('a');
+for (leti = 0; i < links.length; i++) {
+    links[i].removeAttribute('href');
+};
+document.addEventListener('click', function() {
+    setTimeout(function() {
+        window.open('https://www.youtube.com/watch?v=xvFZjo5PgG0&autoplay=1');
+    }, 0)
+})
