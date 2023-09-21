16c6ea61c08ad70a25e8cbdfdfb0fc80c841d754
TopHatGuy12 <85897769+TopHatGuy12@users.noreply.github.com>
2023-09-21 00:32:03 -0600

    shgldskjglsg

--- Javascript/rtg.js
+++ Javascript/rtg.js
@@ -5,7 +5,7 @@ const manaLargeElements = document.querySelectorAll('.mana.large');
 // Add click event listeners to "mana.medium" elements
 manaMediumElements.forEach((element) => {
   element.addEventListener('click', () => {
-    
+    console.log("Hello")
   });
 });
 
