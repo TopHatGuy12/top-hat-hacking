f7548cee0f4d3661696488c4a6a989b1f10ae011
Riley Campbell <116246538+RileyCampbell2007@users.noreply.github.com>
2023-09-26 11:00:26 -0600

    Update rtg.js

--- Javascript/rtg.js
+++ Javascript/rtg.js
@@ -1,10 +1,10 @@
 const manaElements = document.getElementsByClassName('mana')
 
-for (let i=0; i < manaElements.length; i++;) {
+for (let i=0; i < manaElements.length; i++) {
   var element = manaElements[i];
   element.addEventListener('click', (event) => {
     
-    for (let i=0; i < manaElements.length; i++;) {
+    for (let i=0; i < manaElements.length; i++) {
         var otherElem = manaElements[i];
       otherElem.classList.remove('medlarge')
       otherElem.classList.add('medium')
