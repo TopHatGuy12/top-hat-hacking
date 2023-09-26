0e39ce1909eb3d1a0f3dd3bc9d817078c5754a05
Riley Campbell <116246538+RileyCampbell2007@users.noreply.github.com>
2023-09-26 10:53:29 -0600

    Update rtg.js

--- Javascript/rtg.js
+++ Javascript/rtg.js
@@ -1,3 +1,50 @@
+function generateResults(people) {
+    console.log(people)
+    if (!isNaN(parseInt(people))) {
+        const peopleNumber = parseInt(people);
+        console.log(peopleNumber)
+        if (peopleNumber === 1) {
+            return "Solitaire";
+        } else if (peopleNumber < 2) {
+            return "Use a positive number greater than or equal to 2";
+        } else {
+            const magicGames = [];
+            if (peopleNumber >= 2) {
+                magicGames.push('Standard');
+                magicGames.push('Random Standard');
+                magicGames.push('Chaos Deck');
+            }
+            if (peopleNumber >= 3) {
+                magicGames.push('Commander');
+                magicGames.push('Random Commanders');
+                magicGames.push('Archenemy');
+            }
+            if (peopleNumber >= 4 && peopleNumber % 2 === 0) {
+                magicGames.push('Team Standard');
+                magicGames.push('Two Headed Giant');
+                magicGames.push('Commander Archenemy');
+            }
+            if (peopleNumber >= 6 && peopleNumber % 3 === 0) {
+                magicGames.push('Emperor-General');
+            }
+            if (peopleNumber >= 5) {
+                magicGames.push('Bang!');
+                magicGames.push('Star');
+            }
+            if (peopleNumber >= 6) {
+                magicGames.push('Hidden Ally');
+            }
+            if (peopleNumber >= 4) {
+                magicGames.push('Horde');
+            }
+            const randomIndex = Math.floor(Math.random() * magicGames.length);
+            return magicGames[randomIndex];
+        }
+    } else {
+        return 'Please enter a valid number';
+    }
+    }
+
 const manaElements = document.getElementsByClassName('mana')
 
 for (let i=0; i < manaElements.length; i++) {
@@ -12,6 +59,10 @@ for (let i=0; i < manaElements.length; i++) {
     
     event.target.classList.remove('medium');
     event.target.classList.add('medlarge');
+
+    var number = event.target.id[6:7]
+
+    document.getElementById('result').innerHTML=generateResults(number)
 
   });
 }
