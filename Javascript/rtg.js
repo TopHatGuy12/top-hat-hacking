e76f4a6e4628b15b55298c14844a82f00038aaab
Riley Campbell <116246538+RileyCampbell2007@users.noreply.github.com>
2023-09-26 10:41:10 -0600

    Update rtg.js

--- Javascript/rtg.js
+++ Javascript/rtg.js
@@ -1,37 +1,18 @@
-// Get all elements with class "mana.medium" and "mana.large"
-const manaMediumElements = document.querySelectorAll('.mana.medium');
-const manaMedlargeElements = document.querySelectorAll('.mana.medlarge');
+const manaElements = document.getElementsByClassName('mana')
 
-// Add click event listeners to "mana.medium" elements
-manaMediumElements.forEach((element) => {
+for (let i=0; i < manaElements.length; i++) {
+  var element = manaElements[i];
   element.addEventListener('click', () => {
-    // Remove the "medium" class and add the "large" class to the clicked element
+    
+    for (let i=0; i < manaElements.length; i++) {
+        var otherElem = manaElements[i];
+      otherElem.classList.remove('medlarge')
+      otherElem.classList.add('medium')
+    }
+    
     element.classList.remove('medium');
     element.classList.add('medlarge');
 
-    // Set all other "mana.large" elements to "mana.medium"
-    manaMedlargeElements.forEach((medlargeElement) => {
-      if (medlargeElement !== element) {
-        medlargeElement.classList.remove('medlarge');
-        medlargeElement.classList.add('medium');
-      }
-    });
   });
-});
+}
 
-// Add click event listeners to "mana.large" elements
-manaMedlargeElements.forEach((element) => {
-  element.addEventListener('click', () => {
-    // Remove the "large" class and add the "medium" class to the clicked element
-    element.classList.remove('medlarge');
-    element.classList.add('medium');
-
-    // Set all other "mana.large" elements to "mana.medium"
-    manaMedlargeElements.forEach((medlargeElement) => {
-      if (medlargeElement !== element) {
-        medlargeElement.classList.remove('medlarge');
-        medlargeElement.classList.add('medium');
-      }
-    });
-  });
-});
