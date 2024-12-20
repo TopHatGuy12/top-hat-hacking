82d05c5d2dbbb2cf257bf23eb0d64d41b12fa7cb
Riley Campbell <116246538+RileyCampbell2007@users.noreply.github.com>
2024-12-19 22:26:17 -0700

    Update opentype.js

--- Javascript/opentype.js
+++ Javascript/opentype.js
@@ -21,20 +21,20 @@ function typewriterAnimation() {
 
         const typewriterInterval = setInterval(function() {
             if (i >= textLength) {
-                element.innerText = element.dataset.value.slice(0, i + 1);
+                element.innerHTML = element.dataset.value.slice(0, i + 1);
                 clearInterval(typewriterInterval);
                 document.getElementsByClassName('blockingDiv')[0].style.opacity='0'
                 const typerthingblink = setInterval(function() {
                     if (j % 2 == 0) {
-                        element.innerText = element.dataset.value.slice(0, i + 1) + "<span>|</span>";
+                        element.innerHTML = element.dataset.value.slice(0, i + 1) + "<span>|</span>";
                         j++;
                     } else {
-                        element.innerText = element.dataset.value.slice(0, i + 1) + "<span style=\"opacity:0%;\">|</span>";
+                        element.innerHTML = element.dataset.value.slice(0, i + 1) + "<span style=\"opacity:0%;\">|</span>";
                         j++;
                     }
                 }, 700);
             } else {
-                element.innerText = element.dataset.value.slice(0, i + 1) + "<span>|</span>";
+                element.innerHTML = element.dataset.value.slice(0, i + 1) + "<span>|</span>";
                 i++;
             }
         }, 100); // Adjust the interval (in milliseconds) to control the typing speed
